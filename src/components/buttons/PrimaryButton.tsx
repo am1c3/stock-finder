a5b85cc0d16d7cf9b19833fdfe16d158b47399e6
Ionut <ionut@sprybase.com>
2020-05-13 12:35:37 +0300

    add find stocks button

---
+++ src/components/buttons/PrimaryButton.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import colors from '../../constants/colors'
+import fonts from '../../constants/fonts'
+interface Props {
+    text:string;
+    withLink?: boolean;
+    to?:string;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const WrapperWithLink = styled(Link)`
+    text-decoration:none;
+`
+const ButtonWrapper = styled.div`
+    border-radius:5px;
+    padding:15px 100px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    color:white;
+    background-color:${colors.primary};
+    font-family:${fonts.primary};
+    font-size:20px;
+    &:hover {
+        background-color:${colors.primaryDarker};
+    }
+`
+const PrimaryButton: React.FunctionalComponent<Props> = (props) =>  {
+    if(props.withLink) {
+        return <WrapperWithLink to={props.to}>
+            <ButtonWrapper>{props.text}</ButtonWrapper>
+        </WrapperWithLink>
+    } else {
+        return (
+            <ButtonWrapper onClick={props.onClick}>
+                {props.text}
+            </ButtonWrapper>
+        )
+    }
+   
+}
+export default PrimaryButton
